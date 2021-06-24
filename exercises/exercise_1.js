263fe92dfaba8cfea10c78b987149658f847d86b
Danilo Duarte <daniloformer@gmail.com>
2021-06-24 15:40:43 -0300

    começando exercicio-javascript numero 5

--- exercises/exercise_1.js
+++ exercises/exercise_1.js
@@ -1,3 +1,4 @@
+//exercício 1
 let a = 2
 let b = 3
 console.log(a+b)
@@ -5,7 +6,39 @@ console.log(a-b)
 console.log(a*b)
 console.log(a/b)
 console.log(a%b)
+
+//exercício 2
 let c = 5
 let d = 8
-console.log(d >= c)
 
+if (c > d) {
+    console.log(c)
+} else {
+    console.log(d)
+}
+// exercício 3
+let e = 8
+let f = 10
+let g = 6
+
+if (g > f && g > e) {
+    console.log(g)
+}
+else if (f > e) {
+    console.log(f)
+} else {
+    console.log(e)
+}
+// exercício 4
+let valor = 15
+
+if ( valor > 0 ) {
+    console.log("positive")
+}
+else if (valor < 0 ) {
+console.log("negative")
+}
+else {
+    console.log("zero")
+}
+// exercícios 5
