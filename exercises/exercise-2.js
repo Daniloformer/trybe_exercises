93b1ac86f0835b26ffaee75341aa492bb0a36e5f
Danilo Duarte <daniloformer@gmail.com>
2021-06-24 20:09:13 -0300

    fazendo exercicio 5

---
+++ exercises/exercise-2.js
@@ -0,0 +1,35 @@
+//exercício 1 e 2
+//let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+//let soma = 0
+
+//for (let index = 0; index < numbers.length; index += 1) {
+  //  soma = soma + numbers[index]
+   // console.log(soma);
+//}
+//abaixo exercíci 3
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+//let soma = 0
+
+//for (let index = 0; index < numbers.length; index += 1) {
+  //soma = soma + numbers[index]
+
+//}
+//console.log(numbers.length);
+// exercício 4 
+//if (numbers.length > 20) {
+   // console.log("valor maior que 20")
+ //} else {
+     //console.log("valor menor que 20")
+// }
+ // exercício 5
+
+ let maior = 0;
+
+for (let i = 0; i < numbers.length; i++) {
+   if ( nambers[i] > maior ) {
+      maior = nambers[i];
+   }
+}
