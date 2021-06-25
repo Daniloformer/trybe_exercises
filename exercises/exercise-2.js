4fdc70f9aff4d89d2d18809e3ba08cdb50c4e791
Danilo Duarte <daniloformer@gmail.com>
2021-06-25 15:57:59 -0300

    finalizando exercicio 6

--- exercises/exercise-2.js
+++ exercises/exercise-2.js
@@ -4,8 +4,8 @@
 //let soma = 0
 
 //for (let index = 0; index < numbers.length; index += 1) {
-  //  soma = soma + numbers[index]
-   // console.log(soma);
+//  soma = soma + numbers[index]
+// console.log(soma);
 //}
 //abaixo exercíci 3
 
@@ -14,22 +14,39 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 //let soma = 0
 
 //for (let index = 0; index < numbers.length; index += 1) {
-  //soma = soma + numbers[index]
+//soma = soma + numbers[index]
 
 //}
 //console.log(numbers.length);
 // exercício 4 
 //if (numbers.length > 20) {
-   // console.log("valor maior que 20")
- //} else {
-     //console.log("valor menor que 20")
+// console.log("valor maior que 20")
+//} else {
+//console.log("valor menor que 20")
 // }
- // exercício 5
+// exercício 5
 
- let maior = 0;
+// let maior = 0;
 
-for (let i = 0; i < numbers.length; i++) {
-   if ( nambers[i] > maior ) {
-      maior = nambers[i];
+// for (let i = 0; i < numbers.length; i++) {
+//    if (numbers[i] > maior) {
+//       (maior = numbers[i]);
+//    }
+// }
+// console.log(maior)
+
+// exercícios 6  Descubra quantos valores ímpares existem no array e imprima o resultado. Caso não exista nenhum, imprima a mensagem: "nenhum valor ímpar encontrado"
+
+let quantidadeNumeroImpar = 0;
+for (let posicao = 0; posicao < numbers.length; posicao++) {
+   let num = numbers[posicao];
+   if (num % 2 != 0) {
+      quantidadeNumeroImpar += 1;
    }
+}
+if (quantidadeNumeroImpar === 0 ) {
+   console.log("nenhum valor ímpar encontrado");
+
+} else{
+   console.log(quantidadeNumeroImpar);
 }
