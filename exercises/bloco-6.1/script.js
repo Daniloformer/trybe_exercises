38f046cb3a61260d64f7b9da8c66b6cb76087841
Danilo Duarte <daniloformer@gmail.com>
2021-07-17 15:13:17 -0300

    exe.6.1em andamento

---
+++ exercises/bloco-6.1/script.js
@@ -0,0 +1,7 @@
+let estados = { sp: 'São Paulo', rj: 'Rio de Janeiro', mg: 'Minas Gerais' };
+for (const sigla in estados) {
+  const estado = estados[sigla]
+const elemento = document.createElement('option');
+
+}
+
